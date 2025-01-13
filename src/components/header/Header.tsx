6add8361b70a1793b1abf57b31c3e3d8143d2db4
annythingelse <fernandaguimaraes0202@gmail.com>
2025-01-13 14:13:30 -0300

    edit colors

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,13 @@
 import { SearchBar } from "../search-bar/SearchBar";
 import { useSearch } from "../../../utils/SearchContext";
+import "./styles.css";
 
 export const Header = () => {
   const { searchTerm, setSearchTerm } = useSearch();
 
   return (
-    <div>
+    <div className="header-container">
+      <img src="./ghibli-logo.png" alt="logo" />
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
     </div>
   );
